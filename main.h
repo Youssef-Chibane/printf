d6f8a5be4e665d15ad6d516cdc975aade76746a3
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-07-24 13:45:55 -0700

    headear edit

--- main.h
+++ main.h
@@ -6,10 +6,11 @@
 # include <stdlib.h>
 
 int	_printf(const char *format, ...);
-int 	_putchar(char c);
-int 	print_string(char *str);
-void 	print_number_recursive(int n, int *count);
-int 	print_number(int n);
-int 	print_binary(unsigned int n);
+int	handle_format_specifier(char specifier, va_list args);
+int	_putchar(char c);
+int	print_string(char *str);
+void	print_number_recursive(int n, int *count);
+int	print_number(int n);
+int	print_binary(unsigned int n);
 
 #endif
