96cd5f0d7c7b6b235c924d607ee231d30912c609
Youssef-Chibane <chib4ne.youssef@gmail.com>
2023-07-25 10:11:21 -0700

    undo last modif

--- _printf.c
+++ _printf.c
@@ -13,8 +13,8 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if (format == NULL || format[0] == '\0')
-	return (0);
+	if (!format || !format[0])
+		return (-1);
 	while (*format)
 	{
 		if (*format == '%')
